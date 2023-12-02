317fdfcc5dc32586bd36f9bf9f95ed1852cceee2
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-12-02 09:21:41 -0500

    Doc: Model javadoc

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Model is applying a Observer pattern
+ * This is the has-a relationship with the Observer class  
+ */
 public class ExpenseTrackerModel {
 
   //encapsulation - data integrity
@@ -20,7 +24,10 @@ public class ExpenseTrackerModel {
     matchedFilterIndices = new ArrayList<Integer>();
     listeners = new ArrayList<ExpenseTrackerModelListener>();
   }
-
+  /**
+   * Performs the task of adding a transaction to the list of existing transactions
+   * @param t Transaction that is being added
+   */
   public void addTransaction(Transaction t) {
     // Perform input validation to guarantee that all transactions added are non-null.
     if (t == null) {
@@ -31,7 +38,10 @@ public class ExpenseTrackerModel {
     matchedFilterIndices.clear();
     this.stateChanged();
   }
-
+  /**
+   * Function to remove a transaction from the list of transactions
+   * @param t - Transaction to be removed
+   */
   public void removeTransaction(Transaction t) {
     transactions.remove(t);
     // The previous filter is no longer valid.
@@ -39,11 +49,18 @@ public class ExpenseTrackerModel {
     this.stateChanged();
   }
 
+  /**
+   * Returns the current list of existing transactions
+   * @return a list of transactions
+   */
   public List<Transaction> getTransactions() {
     //encapsulation - data integrity
     return Collections.unmodifiableList(new ArrayList<>(transactions));
   }
-
+  /**
+   * Gets a list of all transactions that match the filter indices
+   * @param newMatchedFilterIndices contains the list of indices of matched transactions
+   */
   public void setMatchedFilterIndices(List<Integer> newMatchedFilterIndices) {
       // Perform input validation
       if (newMatchedFilterIndices == null) {
@@ -60,6 +77,10 @@ public class ExpenseTrackerModel {
       this.stateChanged();
   }
 
+  /**
+   * Returns a copy of all the matched filter indices
+   * @return Copy of matched indices
+   */
   public List<Integer> getMatchedFilterIndices() {
       // For encapsulation, copy out the output list
       List<Integer> copyOfMatchedFilterIndices = new ArrayList<Integer>();
@@ -86,6 +107,10 @@ public class ExpenseTrackerModel {
       return true;
   }
 
+  /**
+   * Returns the number of listeners present in the list
+   * @return total number of listeners
+   */
   public int numberOfListeners() {
       // For testing, this is one of the methods.
       //
@@ -93,6 +118,11 @@ public class ExpenseTrackerModel {
       return listeners.size();
   }
 
+  /**
+   * Function to check whether a listener is already present in the list or not
+   * @param listener Listener whose presence is being checked
+   * @return boolean value indicating the result
+   */
   public boolean containsListener(ExpenseTrackerModelListener listener) {
       // For testing, this is one of the methods.
       //
@@ -100,6 +130,9 @@ public class ExpenseTrackerModel {
       return listeners.contains(listener);
   }
 
+  /**
+   * Update the state of the listeners
+   */
   protected void stateChanged() {
       // For the Observable class, this is one of the methods.
       //
